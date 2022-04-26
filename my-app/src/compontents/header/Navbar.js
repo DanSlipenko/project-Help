716ef10d17c77f35235da02365d8efb96d81ffde
Danyil Slipenko <daniel.slipenko@gmail.com>
2022-04-26 13:43:15 -0500

    PC

--- my-app/src/compontents/header/Navbar.js
+++ my-app/src/compontents/header/Navbar.js
@@ -2,9 +2,15 @@ import React from 'react'
 
 function Navbar(){
     return(
-        <>
-            <h2 className='Navbar'>Navbar</h2>
-        </>
+            <div className='Navbar'>
+                <p className='Name'>Name</p>
+                <ul>
+                    <li>Work</li>
+                    <li>Services</li>
+                    <li>About</li>
+                </ul>
+                <button>DONATE</button>
+            </div>
     )
 }
 
